a12c60fb12a4fd25287060d12bcbdd0eed3e4c16
shnorlaxzz <mshnas56@gmail.com>
2023-04-30 17:51:32 +0000

    lab done

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,11 +1,30 @@
 import re  # regular expression
-def read_template (path):
+
+
+def read_template(path):
+    ''' a function that read file from specific path'''
     with open(path) as file:
         content = file.read()
         return content
-def parse_template(content): 
-    lst=[]
-    res = re.findall(r'\{.*?\}', content) 
-    for i in res:
-        lst.append(i.strip("{ }")) 
-    return lst
+
+
+def parse_template(content):
+    '''
+    a function that extract the words between curly braces '{}' inside a string , 
+    it returns two arguments:
+    srtipped: the string after removing the words
+    parts: the words taken from the string
+    '''
+
+    pattern = r"\{([^}]+)\}"
+    stripped = re.sub(pattern, "{}", content)
+    parts = re.findall(pattern, content)
+    return stripped, tuple(parts)
+
+
+def merge(stripped, parts):
+    '''
+    Merges the given parts into the stripped string by replacing any
+    curly-braced placeholders with the corresponding part.
+    '''
+    return stripped.format(*parts)
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,9 +1,9 @@
 import pytest
-from madlib_cli.madlib import read_template, parse_template
-# , merge
+from madlib_cli.madlib import read_template, parse_template, merge
 
 
 def test_read_template_returns_stripped_string():
+    '''a test to check reading files from specific path function --> read_template(path)'''
     actual = read_template("assets/dark_and_stormy_night_template.txt")
     expected = "It was a {Adjective} and {Adjective} {Noun}."
     assert actual == expected
@@ -11,9 +11,11 @@ def test_read_template_returns_stripped_string():
 
 # @pytest.mark.skip("pending")
 def test_parse_template():
+    '''a test to check the parsing process,
+      the function takes a string and return parts which inside curly braces '{}'
+      and the string after extracting them '''
     actual_stripped, actual_parts = parse_template(
-        "It was a {Adjective} and {Adjective} {Noun}."
-    )
+        "It was a {Adjective} and {Adjective} {Noun}.")
     expected_stripped = "It was a {} and {} {}."
     expected_parts = ("Adjective", "Adjective", "Noun")
 
@@ -21,16 +23,17 @@ def test_parse_template():
     assert actual_parts == expected_parts
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_merge():
+    ''' a test to check merge function which merge the arguments with the text'''
     actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
     expected = "It was a dark and stormy night."
     assert actual == expected
 
 
-@pytest.mark.skip("pending")
+# @pytest.mark.skip("pending")
 def test_read_template_raises_exception_with_bad_path():
-
+    ''' a test check the error handling of non existing file path '''
     with pytest.raises(FileNotFoundError):
         path = "missing.txt"
         read_template(path)
